2c71f44db1c6d735167767a257fa948df6048fe9
Ghaidagharaibeh-dot <ghedaghraibeh@yahoo.com>
2021-11-22 22:30:05 +0200

    code ch 31

--- data-structure/hashTable/lib/src/main/java/hashTable/HashTable.java
+++ data-structure/hashTable/lib/src/main/java/hashTable/HashTable.java
@@ -63,7 +63,25 @@ public class HashTable<T> {
     }
 
 
-  
 
+    public String repeatedWord(String text) throws Exception{
+        if (text == null){
+            throw new Exception("Empty Text!!");
+        }
+        String[] words = text.split(" ");
+        String word= "";
+        Map<String,String> wordMap = new HashMap<>();
+
+        for (String s : words) {
+            word = s.toLowerCase();
+            if (wordMap.get(word) != null) {
+                System.out.println("The First Repeated Word Is : " + word);
+                break;
+            } else {
+                wordMap.put(word, word);
+            }
+        }
+        return word;
+    }
 
 }
