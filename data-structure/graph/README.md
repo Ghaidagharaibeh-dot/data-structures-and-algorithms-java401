d27bc8a1a4781ae1188f814edfa3497416e61f94
Ghaidagharaibeh-dot <ghedaghraibeh@yahoo.com>
2021-11-29 14:41:04 +0200

    code challenge35

---
+++ data-structure/graph/README.md
@@ -0,0 +1,44 @@
+# Graphs
+
+A Graph is a non-linear data structure consisting of nodes and edges. The nodes are sometimes also referred to as vertices and the edges are lines or arcs that connect any two nodes in the graph.
+
+
+## Code challenge 35
+
+Implement your own Graph. The graph should be represented as an adjacency list, and should include the following methods:
+
+- add vertex
+
+- add edge
+
+- get vertices
+
+- get neighbors
+
+- size
+
+## Approach and Efficiency
+
+1) add vertex
+
+- Time: O(1)
+- Space: O(1)
+
+2) add edge
+- Time: O(1)
+- Space: O(1)
+
+3) get Vertices
+
+- Time: O(1)
+- Space: O(n) ---> n: is the number of Vertices
+
+3) get neighbors
+
+- Time: O(1)
+- Space: O(n) ---> n: is the number of neighbor Vertices
+
+5) size
+
+- Time: O(n) n: is the number of neighbor Vertices
+- Space: O(1)
---
+++ data-structure/graph/lib/src/main/java/graph/Graph.java
@@ -0,0 +1,48 @@
+package graph;
+
+import java.util.*;
+
+public class Graph {
+    private final Map<Vertex, List<Vertex>> adjVertices;
+
+    public Graph() {
+        adjVertices = new HashMap<>();
+    }
+
+    void addVertex(String data) {
+        Vertex vertex = new Vertex(data);
+        adjVertices.putIfAbsent(vertex, new ArrayList<>());
+    }
+
+    void addEdge(String data1, String data2) {
+        Vertex vertex1 = new Vertex(data1);
+        Vertex vertex2 = new Vertex(data2);
+
+        adjVertices.get(vertex1).add(vertex2);
+        adjVertices.get(vertex2).add(vertex1);
+    }
+
+    String printGraph() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Vertex vertex : adjVertices.keySet()) {
+            stringBuilder.append(vertex);
+            stringBuilder.append(adjVertices.get(vertex));
+        }
+
+        return stringBuilder.toString();
+    }
+
+    int size() {
+        return adjVertices.size();
+    }
+
+    public Set<Vertex> getVertices(){
+        return adjVertices.keySet();
+    }
+
+    public List<Vertex> getNeighbors(String data) {
+        return adjVertices.get(new Vertex(data));
+    }
+
+
+}
