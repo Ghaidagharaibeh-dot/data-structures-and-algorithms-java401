c6c5872144067d6df6b7adf0e97aadc11624328c
Ghaidagharaibeh-dot <ghedaghraibeh@yahoo.com>
2021-12-08 20:15:26 +0200

    code ch 30

---
+++ data-structure/hashTableDataStructure/README.md
@@ -0,0 +1,16 @@
+# Hash Table Implementation
+A hash table is a data structure and it is an array of list. Each list is known as a bucket. The position of the bucket is identified by calling the hashcode() method
+
+# Challenge
+Implement a Hashtable Class
+
+# API
+- ```add(K key, V value)```: add the key and value pair to the table.
+
+- ```get(K key)```: Returns value associated with that key in the table
+
+- ```contains```: Returns boolean, indicating if the key exists in the table already.
+
+- ```remove(K key)```: remove the value from the table based on the key.
+
+-```isEmpty()```: check the hash table is empty or not.
---
+++ data-structure/hashTableDataStructure/app/src/main/java/hashTableDataStructure/HashTable.java
@@ -0,0 +1,133 @@
+package hashTableDataStructure;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class HashTable<K, V> {
+    private ArrayList<HashNode<K, V>> bucketArray;
+
+    private int numBuckets;
+
+    private int size;
+
+    public HashTable() {
+        bucketArray = new ArrayList<>();
+        numBuckets = 30;
+        size = 0;
+
+        for (int index = 0; index < numBuckets; index++) {
+            bucketArray.add(null);
+        }
+    }
+    private int hashCode(K key) {
+        return Objects.hashCode(key);
+    }
+
+    private int getBucketIndex(K key) {
+        int hashCode = hashCode(key);
+
+        int index = hashCode % numBuckets;
+
+        index = index < 0 ? index * -1 : index;
+
+        return index;
+    }
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return getSize() == 0;
+    }
+
+    public void add(K key, V value) {
+
+        int bucketIndex = getBucketIndex(key);
+        int hashCode = hashCode(key);
+
+        HashNode<K, V> head = bucketArray.get(bucketIndex);
+
+        while (head != null) {
+            if (head.key.equals(key) && head.hashCode == hashCode) {
+                head.value = value;
+                return;
+            }
+
+            head = head.next;
+        }
+
+        size++;
+        head = bucketArray.get(bucketIndex);
+        HashNode<K, V> newNode = new HashNode<>(key, value, hashCode);
+        newNode.next = head;
+        bucketArray.set(bucketIndex, newNode);
+
+        if ((1.0 * size) / numBuckets >= 0.7) {
+            ArrayList<HashNode<K, V>> temp = bucketArray;
+            bucketArray = new ArrayList<>();
+            numBuckets = 2 * numBuckets;
+            size = 0;
+
+            for (int index = 0; index < numBuckets; index++) {
+                bucketArray.add(null);
+            }
+
+            for (HashNode<K, V> headNode : temp) {
+                while (headNode != null) {
+                    add(headNode.key, headNode.value);
+                    headNode = headNode.next;
+                }
+            }
+        }
+    }
+
+    public V remove(K key) {
+        int bucketIndex = getBucketIndex(key);
+        int hashCode = hashCode(key);
+        HashNode<K, V> head = bucketArray.get(bucketIndex);
+
+        HashNode<K, V> prev = null;
+        while (head != null) {
+            if (head.key.equals(key) && hashCode == head.hashCode)
+                break;
+
+            prev = head;
+            head = head.next;
+        }
+
+        if (head == null)
+            return null;
+
+        size--;
+
+        if (prev != null)
+            prev.next = head.next;
+        else
+            bucketArray.set(bucketIndex, head.next);
+
+        return head.value;
+    }
+
+    public V get(K key) {
+        int bucketIndex = getBucketIndex(key);
+        int hashCode = hashCode(key);
+
+        HashNode<K, V> head = bucketArray.get(bucketIndex);
+        while (head != null) {
+            if (head.key.equals(key) && head.hashCode == hashCode) {
+                return head.value;
+            }
+
+            head = head.next;
+        }
+        return null;
+    }
+
+    public boolean contains(K key) {
+        if (key == null){
+            throw new IllegalArgumentException("Cannot find a null key.");
+        }
+        return get(key) != null;
+    }
+
+}
